853d434c0c32b8759db0e147e4d3da2cf781b704
jayesh-laptop <work.jayesh@gmail.com>
2019-04-29 19:08:18 -0400

    utils change

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import tensorlayer as tl
-from tensorlayer.layers import Input, Dense, DeConv2d, Reshape, BatchNorm2d, Conv2d, Flatten, BatchNorm
+from tensorlayer.layers import InputLayer, DenseLayer, DeConv2d, Reshape, BatchNorm2d, Conv2d, Flatten, BatchNorm
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -11,8 +11,8 @@ def get_generator(shape, gf_dim=64): # Dimension of gen filters in first conv la
     w_init = tf.glorot_normal_initializer()
     gamma_init = tf.random_normal_initializer(1., 0.02)
 
-    ni = Input(shape)
-    nn = Dense(n_units=(gf_dim * 8 * s16 * s16), W_init=w_init, b_init=None)(ni)
+    ni = InputLayer(shape)
+    nn = DenseLayer(n_units=(gf_dim * 8 * s16 * s16), W_init=w_init, b_init=None)(ni)
     nn = Reshape(shape=[-1, s16, s16, gf_dim*8])(nn)
     nn = BatchNorm(decay=0.9, act=tf.nn.relu, gamma_init=gamma_init, name=None)(nn)
     nn = DeConv2d(gf_dim * 4, (5, 5), (2, 2), W_init=w_init, b_init=None)(nn)
@@ -30,7 +30,7 @@ def get_discriminator(shape, df_dim=64): # Dimension of discrim filters in first
     gamma_init = tf.random_normal_initializer(1., 0.02)
     lrelu = lambda x : tf.nn.leaky_relu(x, 0.2)
 
-    ni = Input(shape)
+    ni = InputLayer(shape)
     nn = Conv2d(df_dim, (5, 5), (2, 2), act=lrelu, W_init=w_init)(ni)
     nn = Conv2d(df_dim*2, (5, 5), (2, 2), W_init=w_init, b_init=None)(nn)
     nn = BatchNorm2d(decay=0.9, act=lrelu, gamma_init=gamma_init)(nn)
@@ -39,6 +39,6 @@ def get_discriminator(shape, df_dim=64): # Dimension of discrim filters in first
     nn = Conv2d(df_dim*8, (5, 5), (2, 2), W_init=w_init, b_init=None)(nn)
     nn = BatchNorm2d(decay=0.9, act=lrelu, gamma_init=gamma_init)(nn)
     nn = Flatten()(nn)
-    nn = Dense(n_units=1, act=tf.identity, W_init=w_init)(nn)
+    nn = DenseLayer(n_units=1, act=tf.identity, W_init=w_init)(nn)
 
     return tl.models.Model(inputs=ni, outputs=nn, name='discriminator')
