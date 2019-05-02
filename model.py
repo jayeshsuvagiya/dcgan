50db523e58e1013acce9e39d3aba538f0aacd030
jayesh-laptop <work.jayesh@gmail.com>
2019-05-01 23:43:57 -0400

    base model

--- model.py
+++ model.py
@@ -7,16 +7,14 @@ FLAGS = flags.FLAGS
 
 def get_generator(shape, gf_dim=128): # Dimension of gen filters in first conv layer. [64]
     image_size = FLAGS.output_size
-    s16 = image_size // 32
+    s16 = image_size // 16
     w_init = tf.glorot_normal_initializer()
     gamma_init = tf.random_normal_initializer(1., 0.02)
 
-    ni = Input(shape)
-    nn = Dense(n_units=(gf_dim * 16 * s16 * s16), W_init=w_init, b_init=None)(ni)
-    nn = Reshape(shape=[-1, s16, s16, gf_dim*16])(nn)
+    ni = InputLayer(shape)
+    nn = DenseLayer(n_units=(gf_dim * 8 * s16 * s16), W_init=w_init, b_init=None)(ni)
+    nn = Reshape(shape=[-1, s16, s16, gf_dim*8])(nn)
     nn = BatchNorm(decay=0.9, act=tf.nn.relu, gamma_init=gamma_init, name=None)(nn)
-    nn = DeConv2d(gf_dim * 8, (5, 5), (2, 2), W_init=w_init, b_init=None)(nn)
-    nn = BatchNorm2d(decay=0.9, act=tf.nn.relu, gamma_init=gamma_init)(nn)
     nn = DeConv2d(gf_dim * 4, (5, 5), (2, 2), W_init=w_init, b_init=None)(nn)
     nn = BatchNorm2d( decay=0.9, act=tf.nn.relu, gamma_init=gamma_init)(nn)
     nn = DeConv2d(gf_dim * 2, (5, 5), (2, 2), W_init=w_init, b_init=None)(nn)
