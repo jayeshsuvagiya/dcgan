421c5bfeac8039eeb4d5122303d1b35df03ca694
jayesh-laptop <work.jayesh@gmail.com>
2019-05-01 23:46:10 -0400

    base model

--- model.py
+++ model.py
@@ -11,8 +11,8 @@ def get_generator(shape, gf_dim=128): # Dimension of gen filters in first conv l
     w_init = tf.glorot_normal_initializer()
     gamma_init = tf.random_normal_initializer(1., 0.02)
 
-    ni = InputLayer(shape)
-    nn = DenseLayer(n_units=(gf_dim * 8 * s16 * s16), W_init=w_init, b_init=None)(ni)
+    ni = Input(shape)
+    nn = Dense(n_units=(gf_dim * 8 * s16 * s16), W_init=w_init, b_init=None)(ni)
     nn = Reshape(shape=[-1, s16, s16, gf_dim*8])(nn)
     nn = BatchNorm(decay=0.9, act=tf.nn.relu, gamma_init=gamma_init, name=None)(nn)
     nn = DeConv2d(gf_dim * 4, (5, 5), (2, 2), W_init=w_init, b_init=None)(nn)
